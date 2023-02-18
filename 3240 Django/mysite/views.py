7c4ff9407c61db9a420a1a9b69c4847fe4d0f244
Andrew-Ceniccola <109996166+Andrew-Ceniccola@users.noreply.github.com>
2023-02-18 16:02:38 -0500

    Delete views.py

--- 3240 Django/mysite/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def homepage(request):
-    NAME = "Homepage"
-    return render(request, 'homepage.html', {'name':NAME})
